0db86eeefc819f51fc295b2b4b4ab3b5184b76ec
Aske Hippe Brun <mail@askehippebrun.com>
2025-02-26 15:54:10 +0100

    klokken

--- src/kodederskalgoereting.js
+++ src/kodederskalgoereting.js
@@ -6,7 +6,7 @@ function opdaterOnline() {
 	console.log("hmm hvor mange er online??")
 
     const randomNumber = tilfaeldigtTalMellemEtOgTusind();
-    const visitorCountElement = document.getElementById("visitor-count");
+    const visitorCountElement = document.getElementById("hvor-mange-er-online?");
     
     if (visitorCountElement) {
         visitorCountElement.textContent = randomNumber;
@@ -16,3 +16,41 @@ function opdaterOnline() {
 }
 
 opdaterOnline();
+
+function visKlokkenTakSoedeComputerIkkeDraebMigNaarDuTagerOver() {
+    const nu = new Date();
+    
+    const tidsformattering = new Intl.DateTimeFormat('da-DK', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    });
+    
+    const datoformattering = new Intl.DateTimeFormat('da-DK', {
+        weekday: 'short',
+        day: 'numeric',
+        month: 'short',
+		year: 'numeric'
+    });
+    
+	const klokkenElement = document.getElementById("klok1");
+	if (klokkenElement) {
+		klokkenElement.textContent = tidsformattering.format(nu);
+	} else {
+		console.error("Element med id 'klok1' blev ikke fundet:(");
+	}
+
+	const datoElement = document.getElementById("dato1");
+	if (datoElement) {
+		datoElement.textContent = datoformattering.format(nu);
+	} else {
+		console.error("Element med id 'dato1' blev ikke fundet:(");
+	}
+}
+
+visKlokkenTakSoedeComputerIkkeDraebMigNaarDuTagerOver();
+
+// Opdater hvert 10. sekund
+setInterval(() => {
+    visKlokkenTakSoedeComputerIkkeDraebMigNaarDuTagerOver();
+}, 10_000);
