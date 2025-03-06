97530388255355fce4efd271394fa17407ee8a3d
Aske Hippe Brun <mail@askehippebrun.com>
2025-03-06 09:17:13 +0100

    gør robotterne glade

--- _config.ts
+++ _config.ts
@@ -7,6 +7,9 @@ import date from "lume/plugins/date.ts";
 import { da } from "npm:date-fns/locale/da";
 import transformImages from "lume/plugins/transform_images.ts";
 import robots from "lume/plugins/robots.ts";
+import ogImages from "lume/plugins/og_images.ts";
+import { read } from "lume/core/utils/read.ts";
+import metas from "lume/plugins/metas.ts";
 
 const site = lume({
 	src: './src',
@@ -21,6 +24,22 @@ site.use(sitemap());
 site.use(date({
 	locales: { da },
 }));
+site.use(ogImages({
+	satori: {
+		fonts: [
+			{
+				name: "georgia",
+				weight: 400,
+				data: await read(
+					"https://github.com/collinhover/learn/raw/refs/heads/master/css/webfonts/georgia.woff",
+					true
+				),
+			},
+		],
+	},
+	cache: true
+}));
+site.use(metas());
 
 site.copy("kodederskalgoereting-2025-03-04.js");
 site.copy("vejret");
