d7ae0de11197ec1be6955d3c6fe7ddbae896bb74
Aske Hippe Brun <mail@askehippebrun.com>
2025-03-13 18:04:36 +0100

    lav "nyt" side

---
+++ src/nyt/index.page.js
@@ -0,0 +1,15 @@
+export const layout = "index-layout-nyt.vto";
+export const titel = "postpost";
+export const openGraphLayout = "/og_billede_normal_side.jsx";
+
+export default function* ({ search, paginate }) {
+  const posts = search.pages("type=opslag udgivet=nej", "dato=desc");
+  const options = {
+    url: (n) => n === 1 ? '/nyt' : `/nyt/side/${n}/`,
+    size: 3,
+  };
+
+  for (const page of paginate(posts, options)) {
+    yield page;
+  }
+}
